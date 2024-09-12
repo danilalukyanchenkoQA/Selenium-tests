23335f557d78e2457a77fefc3ac40eb5166f9388
danilalukyanchenkoQA <danila.kaluga26@gmail.com>
2024-09-12 12:18:09 +0300

    change localization

--- Selenium Lessons.py
+++ Selenium Lessons.py
@@ -1,11 +1,7 @@
 import time
 
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-
-from selenium.webdriver.chrome.service import Service
-service = Service(executable_path=ChromeDriverManager().install())
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+driver = webdriver.Chrome()
 
 
 # Управление навигацией браузера
